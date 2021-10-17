3464fc20f3f8c23baa5e6bd46a59fe68c61123bb
Boboye-Ak <boboye771@gmail.com>
2021-10-17 06:09:15 +0100

    database has been moved online

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const mongoose= require("mongoose")
 const app=express()
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-mongoose.connect("mongodb://localhost/Patients")
+mongoose.connect("mongodb+srv://boboye:boboye@cluster0.r1cxf.mongodb.net/patients?retryWrites=true&w=majority")
 app.use("/", router )
 
 
 
-app.listen(process.env.port||5000,()=>{
+app.listen(process.env.PORT||5000,()=>{
     console.log("Server is listening on port 5000...")
 })
